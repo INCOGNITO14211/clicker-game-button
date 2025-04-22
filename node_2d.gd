b5ec527eb907f67ee296bac5d2e3554ce5322c13
INCOGNITO14211 <tylerjhod828@icloud.com>
2025-04-22 10:02:19 -0500

    I changed the code a little bit

--- node_2d.gd
+++ node_2d.gd
@@ -1,12 +1,12 @@
 extends Node2D
 var prestige_points = 0
 var prestige_multiplier = 1
-var prestige_threshold = 100000
-var score = 0
+var prestige_threshold = 10000
+var score = 100000
 var click_power = 1
 var upgrade_level = click_power
 var upgrade_level2 = auto_click_rate
-var auto_click_rate = 1
+var auto_click_rate = 0
 var scaling_factor=1.5
 var base_cost = 10
 var base_cost2 = 50
@@ -14,7 +14,7 @@ var click_power_cost = base_cost * (scaling_factor ** click_power)
 var auto_clicker_cost = base_cost2 * (scaling_factor ** auto_click_rate)
 
 func _on_button_pressed() -> void:
-	score += click_power
+	score += click_power * prestige_multiplier
 	$ScoreLabel.text = str(int(score))
 	update_ui()
 
@@ -33,7 +33,7 @@ func update_ui():
 func _on_auto_clicker_button_pressed() -> void:
 	if score >= auto_clicker_cost:
 		score -= auto_clicker_cost
-		auto_click_rate += 1
+		auto_click_rate += 5
 		auto_clicker_cost *= 1.3
 		update_ui()
 
@@ -41,5 +41,13 @@ func _on_timer_timeout() -> void:
 	score+=auto_click_rate
 	update_ui()
 
-func prestige():
-	if score >= prestige_threshold
+
+func _on_prestige_button_pressed() -> void:
+	if score >= prestige_threshold:
+		prestige_points += 1 
+		prestige_multiplier += 7 * prestige_points
+		score = 0
+		auto_click_rate = 0
+		click_power = 1
+		scaling_factor = 1.2
+		update_ui()
