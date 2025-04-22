ae50ca1b6fbb4e427d10137c23494b091844bfcd
INCOGNITO14211 <tylerjhod828@iclud.com>
2025-04-21 22:41:28 -0500

    I made stuff

---
+++ node_2d.gd
@@ -0,0 +1,45 @@
+extends Node2D
+var prestige_points = 0
+var prestige_multiplier = 1
+var prestige_threshold = 100000
+var score = 0
+var click_power = 1
+var upgrade_level = click_power
+var upgrade_level2 = auto_click_rate
+var auto_click_rate = 1
+var scaling_factor=1.5
+var base_cost = 10
+var base_cost2 = 50
+var click_power_cost = base_cost * (scaling_factor ** click_power)
+var auto_clicker_cost = base_cost2 * (scaling_factor ** auto_click_rate)
+
+func _on_button_pressed() -> void:
+	score += click_power
+	$ScoreLabel.text = str(int(score))
+	update_ui()
+
+func _on_increase_click_powe_upgrade_pressed() -> void:
+	if score >= click_power_cost:
+		score -= click_power_cost
+		click_power += 1
+		click_power_cost *= 1.125  # Increase cost for the next upgrade
+		update_ui()
+
+func update_ui():
+	$ScoreLabel.text = str(int(score))
+	$ClickPowerButton.text = "Click Power: $" + str(int(click_power_cost))
+	$AutoClickerButton.text = "Auto-Clicker: $" + str(int(auto_clicker_cost))
+
+func _on_auto_clicker_button_pressed() -> void:
+	if score >= auto_clicker_cost:
+		score -= auto_clicker_cost
+		auto_click_rate += 1
+		auto_clicker_cost *= 1.3
+		update_ui()
+
+func _on_timer_timeout() -> void:
+	score+=auto_click_rate
+	update_ui()
+
+func prestige():
+	if score >= prestige_threshold
